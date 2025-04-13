b4bc67da43bca06a0187bd508c5fabbae5441546
Erik Trom <eriktrom@users.noreply.github.com>
2025-04-12 20:36:23 -0700

    refactor: Remove dependent destroy from user association on post model
    - Do not delete a user when a post is deleted.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
 
   validates :title, presence: true, length: { minimum: 3, maximum: 100 }
   validates :content, presence: true, length: { minimum: 20 }
